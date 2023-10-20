79b9369cbf45df32df711b32d8d521fb7bd53ee5
Melody Bugnon <melodybugnon@gmail.com>
2023-10-20 11:11:46 +0200

    ajout valeur dans table

--- imc_python.py
+++ imc_python.py
@@ -4,16 +4,23 @@
 import sqlite3
 con = sqlite3.connect("imc.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE user(user_id, username, first_name, last_name, email, adress, date_created)")
-res = cur.execute("SELECT name FROM sqlite_master")
-res.fetchone()
+#cur.execute("CREATE TABLE user(user_id, username, first_name, last_name, email, adress, date_created)")
+#res = cur.execute("SELECT name FROM sqlite_master") 
+#res.fetchone()
+cur.execute("""
+    INSERT INTO user VALUES
+        (1, "Melobgn", "Melody", "Bugnon", "melodybugnon@gmail.com", "1 Cour Paternotte", 2023-10-20 )
+""")
+con.commit()
+res = cur.execute("SELECT username FROM user")
+res.fetchall()
 
 ## Développez une interface où l'utilisateur pourra saisir son poids (en kg),
  ## sa taille (en cm) et d'autres informations telles que le pseudo, nom, prénom, adresse, etc
 
 pseudo_utilisateur = str(input("Entrez votre pseudo: "))
-nom_utilisateur = str(input("Quel est votre nom de famille? "))
 prenom_utilisateur = str(input("Quel est votre prénom ? "))
+nom_utilisateur = str(input("Quel est votre nom de famille? "))
 adresse_utilisateur = str(input("Quelle est votre adresse? "))
 
 def calcul_imc(masse, taille):
